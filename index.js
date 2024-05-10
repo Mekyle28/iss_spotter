355c4787679c3e84c6011119fd0ba33c0caee2d6
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-10 12:13:53 -0400

    format/ eslint

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 // Fetch the geo coordinates (Latitude & Longitude) for our IP
 // Fetch the next ISS flyovers for our geo coordinates
 
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
 
 
 
 
 
-fetchMyIP((error, ip) => {
-  if (error !== null) {
-    console.log("It didn't work!", error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error !== null) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-  console.log("It worked! Returned IP: ", ip);
-});
+//   console.log("It worked! Returned IP: ", ip);
+// });
--- iss.js
+++ iss.js
@@ -5,7 +5,10 @@ const needle = require('needle');
 
 const fetchMyIP = function(callback) {
   needle.get("https://api.ipify.org?format=json", (error, response, body) => {
-    if (error) return callback(error, null);
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
     if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
@@ -17,17 +20,3 @@ const fetchMyIP = function(callback) {
 };
 
 module.exports = { fetchMyIP };
-
-// my attempt
-// const fetchMyIP = function (callback) {
-//   needle.get("https://api.ipify.org?format=json", (errors, body) => {
-//     const error = (errors);
-//     const ip = (body.body.ip);
-
-//     console.log(ip, typeof ip)
-//     callback(error, ip);
-//   }
-//   );
-// };
-
-// module.exports = { fetchMyIP };
