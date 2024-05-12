4263c609c85aa2b1696528a9aaa95996d22cea52
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-12 00:08:28 -0400

    "add fetchISSFlyOverTimes function"

--- index.js
+++ index.js
@@ -3,17 +3,32 @@
 // Fetch the next ISS flyovers for our geo coordinates
 
 // const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
 
-const ip = "148.170.180.88";
+const { fetchISSFlyOverTimes } = require('./iss');
 
-fetchCoordsByIP(ip, (error, coord) => {
+const obj = { latitude: 42.7729381, longitude: -80.9828842 };
+
+fetchISSFlyOverTimes(obj, (error, data) => {
   if (error !== null) {
     console.log("error", error);
     return;
   }
-  console.log("yay it worked! coordinates:", coord);
+  console.log("yay it worked! we got the rise time and duration:", data);
 });
+
+
+
+
+// const ip = "148.170.180.88";
+
+// fetchCoordsByIP(ip, (error, coord) => {
+//   if (error !== null) {
+//     console.log("error", error);
+//     return;
+//   }
+//   console.log("yay it worked! coordinates:", coord);
+// });
 
 
 
--- iss.js
+++ iss.js
@@ -3,22 +3,23 @@ const needle = require('needle');
 
 
 
-// const fetchMyIP = function(callback) {
-//   needle.get("https://api.ipify.org?format=json", (error, response, body) => {
-//     if (error) {
-//       callback(error, null);
-//       return;
-//     }
+const fetchMyIP = function(callback) {
+  needle.get("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-//     if (response.statusCode !== 200) {
-//       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-//       return;
-//     }
-//     const ip = body.ip;
-//     callback(null, ip);
-//   });
-// };
-// const ip = "148.170.180.88";
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+    const ip = body.ip;
+    callback(null, ip);
+  });
+};
+
+const ip = "148.170.180.88";
 
 
 const fetchCoordsByIP = function(ip, callback) {
@@ -47,7 +48,40 @@ const fetchCoordsByIP = function(ip, callback) {
 
 
 
+// * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+// * Input:
+// *   - An object with keys `latitude` and `longitude`
+// *   - A callback (to pass back an error or the array of resulting data)
+// * Returns (via Callback):
+// *   - An error, if any (nullable)
+// *   - The fly over times as an array of objects (null if error). Example:
+// *     [ { risetime: 134564234, duration: 600 }, ... ]
+// */
+// const obj =  { latitude: 42.7729381, longitude: -80.9828842};
+
+const fetchISSFlyOverTimes = function(coord, callback) {
+  needle.get(`https://iss-flyover.herokuapp.com/json/?lat=${coord.latitude}&lon=${coord.longitude}`, (error, response, body) => {
+    if (error !== null) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const message = `server message says: ${body}`;
+      callback(Error(message), null);
+      return;
+    }
+    const data = body.response;
+    callback(null, data);
+    return;
+  });
+};
+
+//const la = "hey";
+// const la = 42.7729381;
+// const lo = -80.9828842;
+// fetchISSFlyOverTimes(obj, (x) => console.log("error or data", x));
 
 
-module.exports = { fetchCoordsByIP };
+module.exports = { fetchISSFlyOverTimes };
+// module.exports = { fetchCoordsByIP };
 // module.exports = { fetchMyIP };
