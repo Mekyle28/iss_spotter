634e8dab552bc301d39478e6f8866a10b98ebd4d
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-11 23:09:10 -0400

    "add fetchCoordsByIP function"

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
 // Fetch the next ISS flyovers for our geo coordinates
 
 // const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
+const ip = "148.170.180.88";
+
+fetchCoordsByIP(ip, (error, coord) => {
+  if (error !== null) {
+    console.log("error", error);
+    return;
+  }
+  console.log("yay it worked! coordinates:", coord);
+});
 
 
 
--- iss.js
+++ iss.js
@@ -3,20 +3,48 @@ const needle = require('needle');
 
 
 
-const fetchMyIP = function(callback) {
-  needle.get("https://api.ipify.org?format=json", (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
+// const fetchMyIP = function(callback) {
+//   needle.get("https://api.ipify.org?format=json", (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
 
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-      return;
+//     if (response.statusCode !== 200) {
+//       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+//       return;
+//     }
+//     const ip = body.ip;
+//     callback(null, ip);
+//   });
+// };
+// const ip = "148.170.180.88";
+
+
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error !== null) {
+      console.log("error: ", error);
+      return callback(error, null);
     }
-    const ip = body.ip;
-    callback(null, ip);
+    if (!body.success) {
+      const message = (`looks like ${ip} isn't a valid IP address. Please try again with a valid IP`);
+      return callback(Error(message), null);
+    }
+    const lat = body.latitude;
+    const long = body.longitude;
+    const coord = {
+      latitude: lat,
+      longitude: long
+    };
+    return callback(null, coord);
   });
 };
 
-module.exports = { fetchMyIP };
+
+
+
+
+module.exports = { fetchCoordsByIP };
+// module.exports = { fetchMyIP };
